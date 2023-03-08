213dec03b183e821b4e1401873ef58902f5c91bb
6PaTeL <118552281+6PateL@users.noreply.github.com>
2023-03-08 14:07:37 +0300

    1

---
+++ Coroutine without MonoBehaviour/Assets/Scripts/Coroutine without Mono/Test/TestMono.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class TestMono : MonoBehaviour
+{
+    private Tester _tester;
+    private void Start() {
+        this._tester = new Tester(); 
+    } 
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            this._tester.StartTestRoutine();
+        }
+        if (Input.GetKeyDown(KeyCode.W)) {
+            this._tester.StopTestRoutine();   
+        }
+    }
+} 
